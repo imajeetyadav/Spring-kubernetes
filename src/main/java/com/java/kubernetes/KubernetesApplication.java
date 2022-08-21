c03bdeb7cb660ea508bff6729695253ec7c0d2ac
Ajeet yadav <ajeetyadav164@gmail.com>
2022-08-21 14:24:56 +0530

    Dockerfile && Kubernetes

--- src/main/java/com/java/kubernetes/KubernetesApplication.java
+++ src/main/java/com/java/kubernetes/KubernetesApplication.java
@@ -2,9 +2,20 @@ package com.java.kubernetes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @SpringBootApplication
+@RestController
 public class KubernetesApplication {
+
+	@GetMapping(value = "/")
+    public String helloWorld() {
+        String response = "<H1> Hello World   </H1>" + new Date();
+        return response;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(KubernetesApplication.class, args);
